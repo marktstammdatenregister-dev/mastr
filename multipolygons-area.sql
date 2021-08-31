36c9641440b97961f5b7e69060cfd9beef1ac990
curiousleo <curiousleo@users.noreply.github.com>
2021-08-31 11:35:09 +0200

    Store area as REAL but show as INTEGER

--- multipolygons-area.sql
+++ multipolygons-area.sql
@@ -2,7 +2,7 @@
 PRAGMA synchronous=OFF;
 
 -- Precompute area.
-ALTER TABLE multipolygons ADD COLUMN area INTEGER;
-UPDATE multipolygons SET area = CAST(Area(Transform(GEOMETRY, 25832)) AS INTEGER);
+ALTER TABLE multipolygons ADD COLUMN area REAL;
+UPDATE multipolygons SET area = Area(Transform(GEOMETRY, 25832));
 CREATE INDEX idx_multipolygons_area ON multipolygons(area);
 VACUUM;
