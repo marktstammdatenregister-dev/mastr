6dc16cf68d9663f732bbc440a949ec27750b1ee1
curiousleo <curiousleo@users.noreply.github.com>
2021-09-14 13:57:11 +0200

    Add multipolygons_operator index

--- multipolygons-area.sql
+++ multipolygons-area.sql
@@ -5,4 +5,9 @@ PRAGMA synchronous=OFF;
 ALTER TABLE multipolygons ADD COLUMN area REAL;
 UPDATE multipolygons SET area = Area(Transform(GEOMETRY, 25832));
 CREATE INDEX idx_multipolygons_area ON multipolygons(area);
+
+-- Create operator index.
+CREATE INDEX multipolygons_operator ON multipolygons(operator);
+
+-- Cleanup.
 VACUUM;
