733b346b0a81c617be0c7ead389f720645858d63
curiousleo <curiousleo@users.noreply.github.com>
2021-09-15 20:40:00 +0200

    Index on osm_id

--- multipolygons-area.sql
+++ multipolygons-area.sql
@@ -4,10 +4,15 @@ PRAGMA synchronous=OFF;
 -- Precompute area.
 ALTER TABLE multipolygons ADD COLUMN area REAL;
 UPDATE multipolygons SET area = Area(Transform(GEOMETRY, 25832));
+
+-- Create area index: we want to be able to order by area.
 CREATE INDEX idx_multipolygons_area ON multipolygons(area);
 
--- Create operator index.
-CREATE INDEX multipolygons_operator ON multipolygons(operator);
+-- Create operator index: we want to be able to filter by operator.
+CREATE INDEX idx_multipolygons_operator ON multipolygons(operator);
+
+-- Create osm_id index: we want to be able to look up by osm_id.
+CREATE UNIQUE INDEX idx_multipolygons_osm_id ON multipolygons(osm_id);
 
 -- Cleanup.
 VACUUM;
