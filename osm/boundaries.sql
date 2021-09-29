0d0a6600bd5b5161f01d424890951a28cd95cca4
curiousleo <curiousleo@users.noreply.github.com>
2021-09-29 14:03:29 +0200

    Do spatial index properly

--- osm/boundaries.sql
+++ osm/boundaries.sql
@@ -17,13 +17,13 @@ delete from boundaries_import where json_extract(tags, '$.name') is null;
 
 -- Cast EWKB-encoded GEOMETRYCOLLECTION to Multipolygon
 create table boundaries (
-    geometry MULTIPOLYGON not null,
     tags text not null,
     name text generated always as (json_extract(tags, '$.name')) virtual not null,
     admin_level integer generated always as (json_extract(tags, '$.admin_level')) virtual
 );
+select AddGeometryColumn('boundaries', 'geometry', 4326, 'MULTIPOLYGON', 'XY', 1);
 
-insert into boundaries
+insert into boundaries (geometry, tags)
 select
     CastToMultipolygon(GeomFromEWKB(ewkb_hex)),
     tags
@@ -32,7 +32,7 @@ from
 where
     CastToMultipolygon(GeomFromEWKB(ewkb_hex)) is not null;
 
-insert into boundaries
+insert into boundaries (geometry, tags)
 select
     CastToMultipolygon(BuildArea(CastToMultilinestring(GeomFromEWKB(ewkb_hex)))),
     tags
@@ -42,6 +42,7 @@ where
     CastToMultipolygon(GeomFromEWKB(ewkb_hex)) is null;
 
 -- Create index for queries of the form 'select geometry from boundaries where name = ?'
+select CreateSpatialIndex('boundaries', 'geometry');
 create index idx_boundaries_name on boundaries (name);
 
 -- Drop import table
