c472cfc1550f489c5170b393c19d856003c5c28c
curiousleo <curiousleo@users.noreply.github.com>
2021-09-29 14:03:45 +0200

    Simplify fix_ewkb.py

--- osm/fix_ewkb.py
+++ osm/fix_ewkb.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 
-import csv
 import sys
 from shapely import geos, wkb
 
-csv.field_size_limit(2147483647)
 geos.WKBWriter.defaults['include_srid'] = True
-tsv_in = csv.reader(sys.stdin, delimiter='\t')
-tsv_out = csv.writer(sys.stdout, delimiter='\t')
-for record in tsv_in:
-    wkb_in = record[0]
+for line in sys.stdin:
+    wkb_in, tags = line.rstrip().split('\t')
     wkb_out = wkb.loads(bytes.fromhex(wkb_in))
-    tags = record[1]
-    tsv_out.writerow([wkb_out.wkb_hex, tags])
+    print(f"{wkb_out.wkb_hex}\t{tags}")
