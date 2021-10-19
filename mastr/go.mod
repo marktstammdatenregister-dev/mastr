3e91e20938e89efda1d2f939755984d1183bc332
curiousleo <curiousleo@users.noreply.github.com>
2021-10-19 23:23:53 +0200

    Revert "Revert "Insert into PostgreSQL directly""
    This reverts commit 39ef9907bf6740e8e741a83ac4c34f6fce236df6.

--- mastr/go.mod
+++ mastr/go.mod
@@ -2,4 +2,7 @@ module pvdb.de/mastr
 
 go 1.16
 
-require gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+require (
+	github.com/jackc/pgx/v4 v4.13.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
--- mastr/xml2csv.go
+++ mastr/xml2csv.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"encoding/csv"
 	"encoding/xml"
 	"flag"
 	"fmt"
+	"github.com/jackc/pgx/v4"
 	"gopkg.in/yaml.v3"
 	"io"
 	"log"
 	"os"
+	"strconv"
 )
 
 type reference struct {
@@ -182,9 +185,158 @@ func convertXml(td *tableDescriptor, d *xml.Decoder, w *csv.Writer) error {
 	return w.Error()
 }
 
+// Implements CopyFromSource
+type xmlSource struct {
+	root    string
+	element string
+	fields  *fields
+	state   int
+	d       *xml.Decoder
+	values  []string
+	err     error
+}
+
+func newXmlSource(td *tableDescriptor, d *xml.Decoder, fields *fields) xmlSource {
+	return xmlSource{
+		root:    td.Root,
+		element: td.Element,
+		fields:  fields,
+		state:   startRoot,
+		d:       d,
+		values:  nil,
+		err:     nil,
+	}
+}
+
+// Next() implements pgx.CopyFromSource.
+func (s *xmlSource) Next() bool {
+	values, err := s.nextValues()
+	log.Printf("Next %v %v", values, err)
+	if err == io.EOF {
+		return false
+	}
+	if err != nil {
+		s.err = err
+		return false
+	}
+	s.values = values
+	return true
+}
+
+// Values() implements pgx.CopyFromSource.
+func (s *xmlSource) Values() ([]interface{}, error) {
+	values := make([]interface{}, len(s.values))
+	//for i, v := range s.values {
+	//	values[i] = v
+	//}
+
+	// TODO: Convert to the right type dynamically (with pgtype.Record?).
+	values[0], _ = strconv.Atoi(s.values[0])
+	values[1] = s.values[1]
+	log.Printf("Values %v %v %v", s.values, values, s.err)
+	return values, s.err
+}
+
+// Err() implements pgx.CopyFromSource.
+func (s *xmlSource) Err() error {
+	return s.err
+}
+
+func (s *xmlSource) nextValues() ([]string, error) {
+	d := s.d
+	root := s.root
+	element := s.element
+	fields := s.fields
+
+	item := make(map[string]string)
+	var fieldName string
+	var fieldValue []byte
+
+	for {
+		tok, err := d.Token()
+		if err != nil {
+			return nil, err
+		}
+		switch s.state {
+		case startRoot:
+			switch t := tok.(type) {
+			case xml.StartElement:
+				name := xml.StartElement(t).Name.Local
+				if name != root {
+					return nil, fmt.Errorf("[%d] expected start of %s, got %s", s.state, root, name)
+				}
+				s.state = startItemOrEndRoot
+			default: // ignore
+			}
+		case startItemOrEndRoot:
+			switch t := tok.(type) {
+			case xml.StartElement:
+				name := xml.StartElement(t).Name.Local
+				if name != element {
+					return nil, fmt.Errorf("[%d] expected start of %s, got %s", s.state, element, name)
+				}
+				s.state = startFieldOrEndItem
+			case xml.EndElement:
+				name := xml.EndElement(t).Name.Local
+				if name != root {
+					return nil, fmt.Errorf("[%d] expected start of %s, got %s", s.state, root, name)
+				}
+				s.state = finished
+			default: // ignore
+			}
+		case startFieldOrEndItem:
+			switch t := tok.(type) {
+			case xml.StartElement:
+				name := xml.StartElement(t).Name.Local
+				fieldName = name
+				s.state = fieldValueOrEndField
+			case xml.EndElement:
+				name := xml.EndElement(t).Name.Local
+				if name != element {
+					return nil, fmt.Errorf("[%d] expected end of %s, got %s", s.state, element, name)
+				}
+				s.state = startItemOrEndRoot
+				return fields.record(item), nil
+			default: // ignore
+			}
+		case fieldValueOrEndField:
+			switch t := tok.(type) {
+			case xml.StartElement:
+				name := xml.StartElement(t).Name.Local
+				return nil, fmt.Errorf("[%d] expected end of %s, got start of %s", s.state, fieldName, name)
+			case xml.EndElement:
+				name := xml.EndElement(t).Name.Local
+				if name != fieldName {
+					return nil, fmt.Errorf("[%d] expected end of %s, got %s", s.state, fieldName, name)
+				}
+				item[fieldName] = string(fieldValue)
+				fieldValue = []byte{}
+				s.state = startFieldOrEndItem
+			case xml.CharData:
+				fieldValue = append(fieldValue, []byte(xml.CharData(t))...)
+			default: // ignore
+			}
+		case finished:
+			switch tok.(type) {
+			case xml.CharData: // ignore
+			default:
+				return nil, fmt.Errorf("[%d] parsing finished, but got %v", s.state, tok)
+			}
+		}
+	}
+}
+
 func main() {
 	descriptorFileName := flag.String("descriptor", "<undefined>", "file name of the table descriptor")
+	databaseUrl := flag.String("database", "<undefined>", "PostgreSQL database URL")
 	flag.Parse()
+
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, *databaseUrl)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	defer conn.Close(ctx)
 
 	td, err := decodeDescriptor(*descriptorFileName)
 	if err != nil {
@@ -197,8 +349,21 @@ func main() {
 	bw := bufio.NewWriterSize(os.Stdout, bufSize)
 	defer bw.Flush()
 
-	err = convertXml(td, xml.NewDecoder(br), csv.NewWriter(bw))
+	//err = convertXml(td, xml.NewDecoder(br), csv.NewWriter(bw))
+	//if err != nil {
+	//	log.Fatalf("%v", err)
+	//}
+
+	fields := newFields(td.Fields)
+	s := newXmlSource(td, xml.NewDecoder(br), fields)
+	i, err := conn.CopyFrom(
+		ctx,
+		pgx.Identifier{td.Element},
+		fields.header(),
+		&s,
+	)
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
+	log.Printf("wrote %d rows", i)
 }
