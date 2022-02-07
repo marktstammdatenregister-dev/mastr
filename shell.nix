52312c6e1a65d0d311e4da60851230e8683ac0d9
curiousleo <curiousleo@users.noreply.github.com>
2022-02-07 19:17:01 +0100

    Get ready for publishing

--- shell.nix
+++ shell.nix
@@ -3,18 +3,13 @@ pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     axel
     brotli
-    docker-compose
     flyctl
     go
     libxml2
     mustache-go
     niv
-    osmium-tool
-    postgresql_13
-    pup
     pv
     rlwrap
-    spatialite_tools
     sqldiff
     sqlite
     unzip
